12130627ffa23bc6f4c13842a4cf809149074e8c
geniungamartya <geniungamartya@gmail.com>
2023-07-05 13:49:11 +0700

    implement auth plugin

--- src/api/song/routes.js
+++ src/api/song/routes.js
@@ -1,30 +1,44 @@
 const routes = (handler) => [
-    {
-      method: 'POST',
-      path: '/songs',
-      handler: handler.postSongHandler,
+  {
+    method: 'POST',
+    path: '/songs',
+    handler: handler.postSongHandler,
+    options: {
+      auth: 'openmusicapp_jwt',
     },
-    {
-      method: 'GET',
-      path: '/songs',
-      handler: handler.getSongsHandler,
+  },
+  {
+    method: 'GET',
+    path: '/songs',
+    handler: handler.getSongsHandler,
+    options: {
+      auth: 'openmusicapp_jwt',
     },
-    {
-      method: 'GET',
-      path: '/songs/{id}',
-      handler: handler.getSongByIdHandler,
+  },
+  {
+    method: 'GET',
+    path: '/songs/{id}',
+    handler: handler.getSongByIdHandler,
+    options: {
+      auth: 'openmusicapp_jwt',
     },
-    {
-      method: 'PUT',
-      path: '/songs/{id}',
-      handler: handler.putSongByIdHandler,
+  },
+  {
+    method: 'PUT',
+    path: '/songs/{id}',
+    handler: handler.putSongByIdHandler,
+    options: {
+      auth: 'openmusicapp_jwt',
     },
-    {
-      method: 'DELETE',
-      path: '/songs/{id}',
-      handler: handler.deleteSongByIdHandler,
+  },
+  {
+    method: 'DELETE',
+    path: '/songs/{id}',
+    handler: handler.deleteSongByIdHandler,
+    options: {
+      auth: 'openmusicapp_jwt',
     },
-  ];
-  
-  module.exports = routes;
-  
+  },
+];
+
+module.exports = routes;
--- src/server.js
+++ src/server.js
@@ -20,12 +20,19 @@ const users = require('./api/users');
 const UsersService = require('./services/postgres/UsersService');
 const UsersValidator = require('./validator/users');
 
+// authentications
+const authentications = require('./api/authentications');
+const AuthenticationsService = require('./services/postgres/AuthenticationsService');
+const TokenManager = require('./tokenize/TokenManager');
+const AuthenticationsValidator = require('./validator/authentications');
+
 const ClientError = require('./exceptions/ClientError');
 
 const init = async () => {
   const albumService = new AlbumsService();
   const songService = new SongsService();
   const usersService = new UsersService();
+  const authenticationsService = new AuthenticationsService();
 
   const server = Hapi.server({
     port: process.env.PORT,
@@ -35,6 +42,30 @@ const init = async () => {
         origin: ['*'],
       },
     },
+  });
+
+  // registrasi plugin eksternal
+  await server.register([
+    {
+      plugin: Jwt,
+    },
+  ]);
+
+  // mendefinisikan strategy autentikasi jwt
+  server.auth.strategy('openmusicapp_jwt', 'jwt', {
+    keys: process.env.ACCESS_TOKEN_KEY,
+    verify: {
+      aud: false,
+      iss: false,
+      sub: false,
+      maxAgeSec: process.env.ACCESS_TOKEN_AGE,
+    },
+    validate: (artifacts) => ({
+      isValid: true,
+      credentials: {
+        id: artifacts.decoded.payload.id,
+      },
+    }),
   });
 
   await server.register([
@@ -57,6 +88,15 @@ const init = async () => {
       options: {
         service: usersService,
         validator: UsersValidator,
+      },
+    },
+    {
+      plugin: authentications,
+      options: {
+        authenticationsService,
+        usersService,
+        tokenManager: TokenManager,
+        validator: AuthenticationsValidator,
       },
     },
   ]);
