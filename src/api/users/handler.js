f797211ec9995c2756c4f19dfd4452b76c9ee873
geniungamartya <geniungamartya@gmail.com>
2023-07-07 22:55:30 +0700

    fix boilerplate code pada error handling

--- src/api/users/handler.js
+++ src/api/users/handler.js
@@ -1,6 +1,4 @@
-const ClientError = require('../../exceptions/ClientError');
-
-class UsersHandler {
+class UserHandler {
   constructor(service, validator) {
     this._service = service;
     this._validator = validator;
@@ -10,72 +8,31 @@ class UsersHandler {
   }
 
   async postUserHandler(request, h) {
-    try {
-      this._validator.validateUserPayload(request.payload);
-      const {username, password, fullname} = request.payload;
+    this._validator.validateUserPayload(request.payload);
+    const userId = await this._service.addUser(request.payload);
 
-      const userId = await this._service.addUser({username, password, fullname});
-
-      const response = h.response({
-        status: 'success',
-        message: 'User berhasil ditambahkan',
-        data: {
-          userId,
-        },
-      });
-      response.code(201);
-      return response;
-    } catch (error) {
-      if (error instanceof ClientError) {
-        const response = h.response({
-          status: 'fail',
-          message: error.message,
-        });
-        response.code(error.statusCode);
-        return response;
-      }
-
-      // Server ERROR!
-      const response = h.response({
-        status: 'error',
-        message: 'Maaf, terjadi kegagalan pada server kami.',
-      });
-      response.code(500);
-      console.error(error);
-      return response;
-    }
-  }
+    const response = h.response({
+      status: 'success',
+      message: 'User berhasil ditambahkan',
+      data: {
+        userId,
+      },
+    });
+    response.code(201);
+    return response;
+  };
 
   async getUserByIdHandler(request, h) {
-    try {
-      const {id} = request.params;
-      const user = await this._service.getUserById(id);
-      return {
-        status: 'success',
-        data: {
-          user,
-        },
-      };
-    } catch (error) {
-      if (error instanceof ClientError) {
-        const response = h.response({
-          status: 'fail',
-          message: error.message,
-        });
-        response.code(error.statusCode);
-        return response;
-      }
+    const {id} = request.params;
+    const user = await this._service.getUserById(id);
 
-      // server ERROR!
-      const response = h.response({
-        status: 'error',
-        message: 'Maaf, terjadi kegagalan pada server kami.',
-      });
-      response.code(500);
-      console.error(error);
-      return response;
-    }
+    return {
+      status: 'success',
+      data: {
+        user,
+      },
+    };
   }
 }
 
-module.exports = UsersHandler;
+module.exports = UserHandler;
